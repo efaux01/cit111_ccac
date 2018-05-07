b89b966d59594f5ad1463adb73d6e7cae97cd732
efaux01 <eafaux@gmail.com>
2018-05-07 11:13:51 -0400

    Initial upload working verstion AirportSimulator

---
+++ AirportSimulator/src/airportsimulator/Airplane.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airportsimulator;
+
+/**
+ *
+ * @author eliza
+ */
+public class Airplane {
+    private String name;
+    private int seats;
+    private int passengers;
+    private int fuelPercent;
+    private int flightAttendents;
+    private int pilots;
+    private int planeSize;
+    private String airline;
+    private int baggagePercentFull;
+    private String action;
+    private String arrivalTime;
+    private String departureTime;
+    private OtherAirport origin;
+    private OtherAirport destination;
+    
+    public void takeoff(){
+        
+    }//close takeoff
+    
+    public void land(){
+        
+    }//close land
+    
+    public void takeOnFuel(){
+        
+    }//close takeOnFuel
+    
+    public void passengersBoard(){
+        
+    }//close passengersBoard
+    
+    public void passengersExit(){
+        
+    }
+            
+}
